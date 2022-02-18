39c897e2a4b04854a42866026fc240ae0fe44bfb
visor517 <visor517@yandex.ru>
2022-02-18 18:41:30 +0300

    added models and migrations

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class RecipeCategory(models.Model):
+    alias = models.CharField(max_length=16, unique=True, primary_key=True)  # собираюсь использовать в url
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+
+class Recipe(models.Model):
+    category = models.ForeignKey(RecipeCategory, on_delete=models.PROTECT)
+    name = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+
+
+class RecipeImage(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='recipes_images')
